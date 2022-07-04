a2f1c467a1ab6321c345854cb4f4c4f3fda708c4
Corinne Padilla <corinne padilla>
2022-07-03 23:30:28 -0700

    day 1 take home challenge react class 2002

---
+++ take-home-challenge/src/utils/utils.js
@@ -0,0 +1,19 @@
+const API_URL = 'http://www.omdbapi.com';
+const API_KEY = '';
+
+const getMoviesByName = async(name='Batman') => {
+    const response = await fetch(`${API_URL}/?apiKey=${API_KEY}&s=${name}`);
+    const data = await response.json();
+
+    console.log('10 first results of movies:', data);
+}
+
+getMoviesByName('Better call');
+
+const getMoviesDetailsById = async(moviesId) => {
+    const response = await fetch(`${API_URL}/?apitKey=${API_KEY}&i=${moviesId}`);
+    const data = await response.json();
+    console.log('Movie:', data);
+}
+
+getMoviesDetailsById();
