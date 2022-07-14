c4f2a1f16138eee6130c5b42d1ad2ef121e659d9
Corinne Padilla <corinne padilla>
2022-07-13 18:18:37 -0700

    mods to day1 challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,26 @@
+
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import WrappedDetails from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Pet Adoption!</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Link to="/">
+            <h1>Pet Adoption!</h1>
+          </Link>
+          <Routes>
+            <Route path="/details/:id" element={<WrappedDetails />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </StrictMode>
   );
 };
 
 render(<App />, document.getElementById("root"));
-export default App;
--- my-app/src/Pet.js
+++ my-app/src/Pet.js
@@ -1,22 +1,25 @@
+
+import { Link } from "react-router-dom";
+
 const Pet = (props) => {
-    const { name, animal, breed, images, location, id } = props;
-  
-    let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
-    if (images.length) {
-      hero = images[0];
-    }
-  
-    return (
-      <a href={`/details/${id}`}>
-        <div>
-          <image src={hero} alt={name} />
-        </div>
-        <div>
-          <h2>{name}</h2>
-          <h3>{`${animal} - ${breed} - ${location}`}</h3>
-        </div>
-      </a>
-    );
-  };
-  
-  export default Pet;
+  const { name, animal, breed, images, location, id } = props;
+
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <Link to={`/details/${id}`}>
+      <div>
+        <image src={hero} alt={name} />
+      </div>
+      <div>
+        <h2>{name}</h2>
+        <h3>{`${animal} - ${breed} - ${location}`}</h3>
+      </div>
+    </Link>
+  );
+};
+
+export default Pet;
